21c6f42c78476639fb0092e4a0ed960301f329d1
Murod <muradkusherbaev@gmail.com>
2022-11-19 22:44:19 +0500

    navbar created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Typography from "@mui/material/Typography";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./theme";
 import { CssBaseline } from "@mui/material";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Typography backgroundColor="" color="">
+          <Navbar />
           hello form murad
         </Typography>
       </ThemeProvider>
