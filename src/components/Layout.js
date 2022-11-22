29381e23524d5d16b2ae5ca706ecfa24c48c5505
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2022-11-22 21:23:19 +0500

    Make sure before pushing

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,7 +14,6 @@ const Layout = ({ children }) => {
           }}
         >
           <Sidebar />
-          {/* <Outlet sx={{ flexGrow: 1 }} /> */}
           {children}
         </Box>
       </Container>
